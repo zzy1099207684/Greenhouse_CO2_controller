a11d6dda285b55c5352c9ac73a697cb34c2a474e
zhiyo <1099207684@qq.com>
2025-10-02 21:50:02 +0300

    change for needs

--- src/main.cpp
+++ src/main.cpp
@@ -27,10 +27,10 @@ void controller_scan_wifi(void *param) {
     auto *ts = static_cast<thing_speak *>(param);
     EventBits_t b = xEventGroupWaitBits(ts->get_co2_wifi_scan_event_group(), WIFI_INIT_BIT, pdFALSE, pdTRUE, portMAX_DELAY); // wait for wifi init success
     if (b & WIFI_INIT_BIT) {
-        // controller scan wifi ssid
-        thing_speak_service::scan_wifi_ssid_arr(param);
-        auto ssids = ts->get_wifi_scan_result(); // wifi scan
-        if (ts->get_wifi_ssid_index() == -1) {
+        thing_speak_service::scan_wifi_ssid_arr(param); // scan wifi ssid array
+        auto ssids = ts->get_wifi_scan_result(); // get wifi ssid array
+
+        if (ts->get_wifi_ssid_index() == -1) { // just for test
             for (int i = 0; i < 9; i++) {
                 if (ssids[i][0] != '\0') {
                     printf("%s\n", ssids[i]);
@@ -46,23 +46,21 @@ int main() {
     stdio_init_all();
 
     thing_speak ts;
-    // create a event group for co2 level change notification
     EventGroupHandle_t wifi_init_success_group = xEventGroupCreate();
     ts.set_co2_wifi_scan_event_group(wifi_init_success_group); // set event group
 
     xTaskCreate(wifi_init, "wifi_init", 256, &ts, 1, nullptr); // create wifi init task
 
-    xTaskCreate(controller_scan_wifi, "controller_scan_wifi", 256, &ts, 1, nullptr); // controller scan wifi ssid task
+    xTaskCreate(controller_scan_wifi, "controller_scan_wifi", 256, &ts, 1, nullptr); // controller scan wifi ssid task example
 
     ts.set_ssid("Redmi_138D");
     ts.set_pwd("zzyzmy20272025888");
 
-    // xTaskCreate(thing_speak_service::start, "wifi_init", 256, &ts, 1, nullptr); // create wifi init task
+    xTaskCreate(thing_speak_service::start, "wifi_init", 256, &ts, 1, nullptr); // create wifi init task
 
     vTaskStartScheduler();
 
 
-    // xTaskCreate(thing_speak_service::start, "thing_speak_service::start", 256, &ts, 2, nullptr); // create thing speak service task
 
     for (;;) {}
 }
