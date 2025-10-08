acfcce24c14a90e24a4be7892ff1a018edf6b3dc
Sheng Tai <shengtai25@outlook.com>
2025-10-08 13:18:15 +0300

    fix: refine control logic: doesn't need to emergency during the MIXING state since it checks every time the task runs. refactored: Update CO2Controller to be more simplified

--- src/CO2_Controller/CO2Controller.cpp
+++ src/CO2_Controller/CO2Controller.cpp
@@ -109,7 +109,6 @@ void CO2Controller::mixingTimerCallback(TimerHandle_t xTimer)
 }
 
 // this version will only ventilate at emergency situation
-#ifndef WITH_ACTIVE_VENTILATION
 void CO2Controller::controlTask(void* pvParameters)
 {
     auto* self = static_cast<CO2Controller*>(pvParameters);
@@ -169,9 +168,16 @@ void CO2Controller::controlTask(void* pvParameters)
             // CO2 value is above the setpoint + ventilation start threshold. i.e. the CO2 is too high
             else if (CO2_value > CO2_setpoint + VENTILATION_THRESHOLD)
             {
-                valve.close(); // ensure valve is closed
-                Debug::println("CO2 level %.2f >> setpoint %.2f ppm. Too high", CO2_value, CO2_setpoint);
+#ifdef WITH_ACTIVE_VENTILATION
+                controller_state = VENTILATING;
+                valve.close();
+                Debug::println("CO2 level %.2f >> setpoint %.2f ppm. Too high, starting ventilation.",
+                               CO2_value, CO2_setpoint);
+#else
+                valve.close();
+                Debug::println("CO2 level %.2f >> setpoint %.2f ppm.", CO2_value, CO2_setpoint);
                 Debug::println("But who cares, it will drop anyway ( (╯°□°)╯︵ ┻━┻.");
+#endif
             }
             else
             {
@@ -187,134 +193,10 @@ void CO2Controller::controlTask(void* pvParameters)
             }
             break;
         case VENTILATING:
-            // Will never enter this state
-            // This version does not have active ventilation
-            // Only emergency state will turn on the fan
-            break;
-        case INJECTING:
-            // do nothing, wait for timer to close valve
-            break;
-        case MIXING:
-            // check for emergency first
-            if (CO2_value > CO2_CRITICAL && controller_state != EMERGENCY)
-            {
-                valve.close();
-                fan.setSpeed(FAN_SPEED_MAX);
-                xTimerStop(closeValveTimerHandle, 0);
-                xTimerStop(mixingTimerHandle, 0);
-                controller_state = EMERGENCY;
-                xEventGroupSetBits(self->event_group, CO2_WARNING);
-                Debug::println("CO2 level critical: %.2f ppm! Entering EMERGENCY state.", CO2_value);
-            }
-            break;
-        case EMERGENCY:
+#ifdef WITH_ACTIVE_VENTILATION
+            // when the co2 level is much higher than the setpoint
+            // i.e. higher than setpoint + ventilation threshold
             // start ventilation until CO2 is back to the setpoint (with deadband)
-            if (CO2_value <= CO2_setpoint + DEADBAND) // back to normal range + deadband since it drops naturally
-            {
-                fan.setSpeed(0);
-                controller_state = IDLE;
-                Debug::println("CO2 level normal: %.2f ppm. Exiting EMERGENCY state.", CO2_value);
-                xEventGroupClearBits(self->event_group, CO2_WARNING);
-            }
-            else
-            {
-                float diff = CO2_value - (CO2_setpoint + DEADBAND);
-                int speed = static_cast<int>(diff * FAN_SPEED_K);
-                if (speed > FAN_SPEED_MAX)
-                {
-                    speed = FAN_SPEED_MAX;
-                }
-                else if (speed < FAN_SPEED_MIN)
-                {
-                    speed = FAN_SPEED_MIN;
-                }
-                fan.setSpeed(speed);
-            }
-            break;
-        }
-    }
-}
-#else
-// this version will do the ventilation when
-// the gap between setpoint and current value is too large (a parameter can be set, by default 200)
-// so it will actively ventilate
-void CO2Controller::controlTask(void* pvParameters)
-{
-    auto* self = static_cast<CO2Controller*>(pvParameters);
-    auto& sensor = self->sensor;
-    auto& fan = self->fan;
-    auto& valve = self->valve;
-    auto& CO2_value = self->CO2_value;
-    auto& CO2_setpoint = self->CO2_setpoint;
-    auto& controller_state = self->controller_state;
-    auto& closeValveTimerHandle = self->closeValveTimerHandle;
-    auto& mixingTimerHandle = self->mixingTimerHandle;
-
-    while (true)
-    {
-        // control loop every 1 second
-        vTaskDelay(pdMS_TO_TICKS(CONTROL_INTERVAL_MS));
-        CO2_value = sensor.getCO2Level(); // update CO2 value
-
-        // check for emergency first
-        if (CO2_value > CO2_CRITICAL && controller_state != EMERGENCY)
-        {
-            valve.close();
-            fan.setSpeed(FAN_SPEED_MAX);
-            xTimerStop(closeValveTimerHandle, 0);
-            xTimerStop(mixingTimerHandle, 0);
-            controller_state = EMERGENCY;
-            xEventGroupSetBits(self->event_group, CO2_WARNING);
-            Debug::println("CO2 level critical: %.2f ppm! Entering EMERGENCY state.", CO2_value);
-        }
-
-        switch (controller_state)
-        {
-        case IDLE:
-            // CO2 value is below the setpoint
-            if (CO2_value < CO2_setpoint - DEADBAND)
-            {
-                fan.setSpeed(0); // ensure fan is off
-                Debug::println("CO2 level %.2f < setpoint-deadband %.2f ppm. Preparing for injection.",
-                               CO2_value, CO2_setpoint - DEADBAND);
-                float diff = CO2_setpoint - CO2_value + DEADBAND; // inject to upper bound since it drops naturally
-                // calculate valve open time, in ms
-                int open_time = static_cast<int>(diff * VALVE_OPEN_TIME_K);
-                if (open_time > VALVE_OPEN_TIME_MAX)
-                {
-                    open_time = VALVE_OPEN_TIME_MAX;
-                }
-                else if (open_time < VALVE_OPEN_TIME_MIN)
-                {
-                    open_time = VALVE_OPEN_TIME_MIN;
-                }
-                xTimerChangePeriod(closeValveTimerHandle, pdMS_TO_TICKS(open_time), 0);
-                xTimerStart(closeValveTimerHandle, 0);
-                valve.open();
-                controller_state = INJECTING;
-                Debug::println("Injecting CO2 for %d ms.", open_time);
-            }
-            // CO2 value is above the setpoint + ventilation start threshold. i.e. the CO2 is too high
-            else if (CO2_value > CO2_setpoint + VENTILATION_THRESHOLD)
-            {
-                controller_state = VENTILATING;
-                valve.close(); // ensure valve is closed
-                Debug::println("CO2 level %.2f >> setpoint %.2f ppm. Too high, starting ventilation.",
-                               CO2_value, CO2_setpoint);
-            }
-            else
-            {
-                // within deadband, do nothing
-                // need to ensure everything is off and normal
-                // in case someone cut the power during ventilation or injection and it will stay on
-                valve.close();
-                fan.setSpeed(0);
-                xTimerStop(closeValveTimerHandle, 0);
-                xTimerStop(mixingTimerHandle, 0);
-                xEventGroupClearBits(self->event_group, CO2_WARNING);
-            }
-            break;
-        case VENTILATING:
             if (CO2_value <= CO2_setpoint + DEADBAND) // stops at upper bound since it drops naturally
             {
                 fan.setSpeed(0);
@@ -335,13 +217,19 @@ void CO2Controller::controlTask(void* pvParameters)
                 }
                 fan.setSpeed(speed);
             }
-
+#else
+            // This version does not have active ventilation
+            // Only emergency state will turn on the fan
+#endif
             break;
         case INJECTING:
             // do nothing, wait for timer to close valve
             break;
         case MIXING:
+            // check for emergency first
             // do nothing, wait for timer to end mixing
+            // BTW (emergency check is already done before switch statement)
+
             break;
         case EMERGENCY:
             // start ventilation until CO2 is back to the setpoint (with deadband)
@@ -370,4 +258,3 @@ void CO2Controller::controlTask(void* pvParameters)
         }
     }
 }
-#endif
